2bb313e0d3f9bc9ee558372c13f2aed8babee581
ynwa <ynwa@umich.edu>
2020-05-09 17:33:12 +0200

    3sum - working solution - really slow

--- Hash: 3Sum (15) Medium.cpp
+++ Hash: 3Sum (15) Medium.cpp
@@ -17,6 +17,75 @@ using namespace std;
 
 class Solution {
 public:
+	
+	unordered_map<int,vector<int>> valToIndex;
+	
+	bool we_have_already_seen_this_fixed_elem(int fixedVal, int index_of_fixed_elem){
+		
+		auto iter_to_fixed = valToIndex.find(fixedVal);
+		if (iter_to_fixed->second[0] < index_of_fixed_elem) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	bool duplicate_temp_exists_in_given_range(int tempVal, int indexOfTemp, int fixedIndex){
+		
+		
+		auto iter_to_temp = valToIndex.find(tempVal);
+		if(iter_to_temp->second[0] < fixedIndex)
+			return true;
+		
+		if(iter_to_temp->second.size() == 1)
+			return false;
+		
+		else{
+			for(int i = 0; i < iter_to_temp->second.size(); ++i){
+				if(iter_to_temp->second[i] > fixedIndex && iter_to_temp->second[i] < indexOfTemp)
+					return true;
+				if(iter_to_temp->second[i] > indexOfTemp)
+					break;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	//returns true if there is a complement to the left of temp such that complement is not the fixed elem.
+	bool duplicate_complement_exists_to_the_left_of_temp(unordered_map<int, vector<int>>::iterator iter_to_compl, int indexOfTemp, int indexOfFixed){
+		
+		if(iter_to_compl->second[0] < indexOfFixed)
+			return true;
+		
+		for(int i = 0 ; i < iter_to_compl->second.size(); ++i){
+			if(iter_to_compl->second[i] > indexOfFixed && iter_to_compl->second[i] < indexOfTemp)
+				return true;
+			if(iter_to_compl->second[i] > indexOfTemp)
+				break;
+		}
+		
+		return false;
+	}
+	
+	bool complement_exists_to_the_right_of_temp(unordered_map<int, vector<int>>::iterator iter_to_compl, int indexOfTemp, int indexOfFixed){
+		
+		bool flag = false;
+		
+		for(int i = 0; i < iter_to_compl->second.size(); ++i){
+			
+			if(iter_to_compl->second[i] > indexOfTemp){
+				flag = true;
+				break;
+			}
+			
+		}
+		
+		
+		return flag;
+	}
+	
 	vector<vector<int>> threeSum(vector<int>& nums) {
 		
 		if(nums.size() < 3)
@@ -25,7 +94,7 @@ public:
 		
 		vector<vector<int>> triplets;
 		
-		unordered_map<int,vector<int>> valToIndex;
+		
 		//each value from nums vec is mapped with a vec of indices containing that elem.
 		
 		
@@ -49,110 +118,46 @@ public:
 			
 			for(int indexOfTempElem = indexOfFixedElem + 1; indexOfTempElem < nums.size()-1; ++indexOfTempElem){
 				
+				if(we_have_already_seen_this_fixed_elem(nums[indexOfFixedElem], indexOfFixedElem))
+					continue;
+				
 				int sumOfFixedAndTemp = nums[indexOfFixedElem] + nums[indexOfTempElem];
 				//complement is -1 * sumOfFixedAndTemp
 				
-				auto complement_iter = valToIndex.find(-1*(sumOfFixedAndTemp));
-				if(complement_iter == valToIndex.end())
+				int complement = -1*sumOfFixedAndTemp;
+				
+				auto compl_iter = valToIndex.find(complement);
+				
+				if(compl_iter == valToIndex.end())
+					continue; //complement doesn't exist.
+				
+				
+				//if a complement does exist:
+				
+				//if there is only one instance of complement and it is to the left of temp elem.
+				if(compl_iter->second.size() == 1 && compl_iter->second[0] < indexOfTempElem)
 					continue;
 				
-				else{
-					
-					//checking condition 1:
-					auto iter_to_temp_val = valToIndex.find(nums[indexOfTempElem]);
-					bool duplicate_temp_exists = false;
-					
-					for(int i = 0; i < iter_to_temp_val->second.size(); ++i){
-						
-						if(iter_to_temp_val->second[i] > indexOfFixedElem && iter_to_temp_val->second[i] < indexOfTempElem){
-							duplicate_temp_exists = true;
-							break; //duplicate_temp_exists in b/w curr fixed elem and curr temp elem.
-						}
-						
-						if(iter_to_temp_val->second[i] >= indexOfTempElem)
-							break;
-
-					}//this for loop iterates thru the vector of indices that contain the temp elem.
-					
-					if(duplicate_temp_exists)
-						continue; //increment temp elem.
-					
-					else if(not duplicate_temp_exists){
-						
-						if(complement_iter->second[0] < indexOfFixedElem)
-							continue;
-						
-						if(complement_iter->second[0] == indexOfFixedElem && complement_iter->second.size()==1)
-							continue;
-						
-						if(complement_iter->second[0] == indexOfTempElem && complement_iter->second.size() == 1)
-							continue;
-						
-						if(complement_iter->second[0] > indexOfTempElem){
-							//if we pass condition 2: if the first instance of compl is to the right of temp elem.
-							triplets.push_back({nums[indexOfFixedElem], nums[indexOfTempElem], -1*sumOfFixedAndTemp});
-							
-						}//basic way to pass condition 2
-						
-						//other ways to pass condition 2:
-						//if there was no instance of compl in b/w curr fixed elem and curr temp elem, but there
-						//...is an instance of compl to the right of curr temp elem.
-						
-						else{
-							
-							bool duplicate_compl_exists = false;
-							bool complement_exists_to_rightoftemp = false;
-							for(int i = 0; i < complement_iter->second.size(); ++i){
-								
-								if(complement_iter->second[i] < indexOfTempElem && complement_iter->second[i] > indexOfFixedElem){
-									duplicate_compl_exists = true;
-									break;
-								}
-								
-								if(complement_iter->second[i] > indexOfTempElem){
-									complement_exists_to_rightoftemp = true;
-									break;
-								}
-							}//for that goes thru all indices where complement resides
-							//...and looks for duplicate compl in b/w fixed elem and temp elem.
-							//..this for loop also looks for whether there exists a complement to the right of temp.
-							
-							
-							if(duplicate_compl_exists)
-								continue;
-							
-							else if(not duplicate_compl_exists && complement_exists_to_rightoftemp){
-								triplets.push_back({nums[indexOfFixedElem], nums[indexOfTempElem], -1 * sumOfFixedAndTemp});
-							}
-							
-						}//else if basic condition 2 wasn't passed (there are > 1 checks for condition 2)
-						
-					}//else if condition 1 is passed.
-					
-					
-					
-				}/*if a complement does exist, you wanna check two things:
-				  1) say the current temp num is 2, and the curr fixed num is -1, then u wanna check whether
-				  		is already a 2 to the left of this temp 2 such that that 2 is not the fixed elem. aka,
-						you wanna check whether there is a 2 in b/w the fixed elem and the temp, cuz if there is,
-				  		then u can just "continue" outta the loop and save urself a duplicate case.
-				  
-				  2) if condition 1 is false, aka there is no 2 in b/w fixed and temp, then u wanna check
-					 whether there is a complement (in this case the compl is -1 cuz -1 + 2 + (-1) == 0). Specifically,
-				  	you wanna
-				  	 check whether the first instance of the complement is to the right of the curr temp elem. this
-				  	 is needed to save urself from a duplicate case once again. cuz if one instance of the compl is
-				  	 to the left of the curr temp elem, then that would already have been considered and saved as
-					 a triplet*/
-					
+	
+				//has this this temp value already been seen in b/w fixed elem and this temp elem.
+				if(duplicate_temp_exists_in_given_range(nums[indexOfTempElem], indexOfTempElem, indexOfFixedElem))
+					continue;
 				
-			}//inner for loop goes thru all elems to the right of the fixed elem.
+				//if a duplicate temp doesnt exist in the given range.
+				
+				if(duplicate_complement_exists_to_the_left_of_temp(compl_iter, indexOfTempElem, indexOfFixedElem))
+					continue;
+				
+				if(complement_exists_to_the_right_of_temp(compl_iter, indexOfTempElem, indexOfFixedElem))
+					triplets.push_back({nums[indexOfFixedElem], nums[indexOfTempElem], complement});
+				
+				
+			}//inner for loop
 			
-			
-		}//outer for loop, changes the fixed elem. stops at the third last elem.
+		}//outer for loop
 		
 		
-		
+		valToIndex.clear();
 		return triplets;
 		
 	}
@@ -160,6 +165,8 @@ public:
 
 
 int main(){
+	
+	vector<int> testKosi = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6};
 	
 	
 	vector<int> testOne = {-1, 0, 1, 2, -1, -4};
@@ -173,12 +180,14 @@ int main(){
 	vector<int> testSeven = {-2,0,0,2,2};
 	
 	Solution x;
+	auto kosi = x.threeSum(testKosi);
+	
 	
 	auto resultSeven = x.threeSum(testSeven);
 	
-//	for(int i = 0; i < resultSeven.size(); ++i){
+//	for(int i = 0; i < kosi.size(); ++i){
 //		for(int inner = 0; inner < 3; ++inner){
-//			cout << resultSeven[i][inner] << " ";
+//			cout << kosi[i][inner] << " ";
 //		}
 //		cout << "\n";
 //	}
@@ -191,9 +200,9 @@ int main(){
 	auto resultTrois = x.threeSum(testThree);
 	auto resultQuatre = x.threeSum(testFour);
 	
-	for(int i = 0; i < resultSix.size(); ++i){
+	for(int i = 0; i < kosi.size(); ++i){
 		for(int inner = 0; inner < 3; ++inner){
-			cout << resultSix[i][inner] << " ";
+			cout << kosi[i][inner] << " ";
 		}
 		cout << "\n";
 	}
